4ec26d4aa90ffae5e66fbe06cb835998fc80803b
Eri <alex-eri@ya.ru>
2019-08-26 17:12:45 +0300

    kassa

--- kassa.py
+++ kassa.py
@@ -1,3 +1,9 @@
+import pyshtrih
+
+def discovery_callback(*a,**kw):
+    print(a)
+    print(kw)
+
 class Kassa:
     def __init__(self, port, rate):
         devices = pyshtrih.discovery(discovery_callback, port, rate)
@@ -7,13 +13,13 @@ class Kassa:
         device.connect()
         self.device = device
 
-    def bill(row):
+    def bill(self, row):
         s = row['sum']
         s = int(s) * 100
         a = row['account']
-        device.open_check(0)
-        device.sale(
+        self.device.open_check(0)
+        self.device.sale(
             (u'Оплата по л/с %s' % a, 1, s)
         )
-        device.close_check(s)
+        self.device.close_check(s)
 
