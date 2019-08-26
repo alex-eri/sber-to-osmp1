e5f8a54c12ae675b8cc9bc4fd3244d1b31d4852b
Eri <alex-eri@ya.ru>
2019-08-26 18:00:02 +0300

    cashier

--- kassa.py
+++ kassa.py
@@ -17,9 +17,12 @@ class Kassa:
         s = row['sum']
         s = int(s) * 100
         a = row['account']
+
+        cashier = pyshtrih.FD({1021: u'Кассир: Лебедева С.Н.'})
         self.device.open_check(0)
+        self.device.send_tlv_struct(cashier.dump())
         self.device.sale(
-            (u'Расчетный счёт %s' % a, 1000, s)
+            (u'Расчетный счёт л/с%s' % a, 1000, s)
         )
         self.device.close_check(payment_type4=s)
 
